53f355c32dfc17eb3eecff20be5eb6bfe253c8ca
Julian Padron <julian.padronnunez03@gmail.com>
2025-09-16 11:11:07 -0500

    Initial commit

---
+++ backend-java/src/main/java/com/project/recommendation_engine/RecommendatioEngineApplication.java
@@ -0,0 +1,13 @@
+package com.project.recommendation_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendatioEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendatioEngineApplication.class, args);
+	}
+
+}
